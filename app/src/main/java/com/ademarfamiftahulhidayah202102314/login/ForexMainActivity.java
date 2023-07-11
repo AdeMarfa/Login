2ff9d2a305b2939d1e51100496287162390354d9
201103649 Aulia Berliana <auliaaberliana@gmail.com>
2023-07-11 10:18:05 +0700

    Forex

---
+++ app/src/main/java/com/ademarfamiftahulhidayah202102314/login/ForexMainActivity.java
@@ -0,0 +1,108 @@
+package com.ademarfamiftahulhidayah202102314.login;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.ademarfamiftahulhidayah202102314.login.R;
+
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.text.DecimalFormat;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ForexMainActivity extends AppCompatActivity {
+    private ProgressBar loadingProgressBar;
+    private SwipeRefreshLayout swipeRefreshLayout1;
+    private TextView audTextView, bndTextView, btcTextView, eurTextView, gbpTextView, hkdTextView, inrTextView, jpyTextView, myrTextView, usdTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forex_main);
+
+        swipeRefreshLayout1 = findViewById(R.id.swipeRefreshLayout1);
+        audTextView = findViewById(R.id.audTextView);
+        bndTextView = findViewById(R.id.bndextView);
+        btcTextView = findViewById(R.id.btcTextView);
+        eurTextView = findViewById(R.id.eurTextView);
+        gbpTextView = findViewById(R.id.gbpTextView);
+        hkdTextView = findViewById(R.id.hkdTextView);
+        inrTextView = findViewById(R.id.inrTextView);
+        jpyTextView = findViewById(R.id.jpyTextView);
+        myrTextView = findViewById(R.id.myrTextView);
+        usdTextView = findViewById(R.id.usdTextView);
+        loadingProgressBar = findViewById(R.id.loadingProgressBar);
+
+        initSwipeRefreshLayout();
+        initForex();
+    }
+
+    private void initForex() {
+        loadingProgressBar.setVisibility(TextView.VISIBLE);
+
+        String url = "https://openexchangerates.org/api/latest.json?app_id=e54d79084f56452898e94b0e64e12870";
+
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        asyncHttpClient.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Gson gson = new Gson();
+                ForexRootModel rootModel = gson.fromJson(new String(responseBody), ForexRootModel.class);
+                ForexRatesModel ratesModel = rootModel.getRatesModel();
+
+                double aud = ratesModel.getIDR() / ratesModel.getAUD();
+                double bnd = ratesModel.getIDR() / ratesModel.getBND();
+                double btc = ratesModel.getIDR() / ratesModel.getBTC();
+                double eur = ratesModel.getIDR() / ratesModel.getEUR();
+                double gbp = ratesModel.getIDR() / ratesModel.getGBP();
+                double hkd = ratesModel.getIDR() / ratesModel.getHKD();
+                double inr = ratesModel.getIDR() / ratesModel.getINR();
+                double jpy = ratesModel.getIDR() / ratesModel.getJPY();
+                double myr = ratesModel.getIDR() / ratesModel.getMYR();
+                double idr = ratesModel.getIDR();
+
+                audTextView.setText(formatNumber(aud, "###,##0.##"));
+                bndTextView.setText(formatNumber(bnd, "###,##0.##"));
+                btcTextView.setText(formatNumber(btc, "###,##0.##"));
+                eurTextView.setText(formatNumber(eur, "###,##0.##"));
+                gbpTextView.setText(formatNumber(gbp, "###,##0.##"));
+                hkdTextView.setText(formatNumber(hkd, "###,##0.##"));
+                inrTextView.setText(formatNumber(inr, "###,##0.##"));
+                jpyTextView.setText(formatNumber(jpy, "###,##0.##"));
+                myrTextView.setText(formatNumber(myr, "###,##0.##"));
+                usdTextView.setText(formatNumber(idr, "###,##0.##"));
+
+                loadingProgressBar.setVisibility(TextView.INVISIBLE);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+
+                loadingProgressBar.setVisibility(TextView.INVISIBLE);
+            }
+        });
+
+    }
+
+    private void initSwipeRefreshLayout() {
+        swipeRefreshLayout1.setOnRefreshListener(() -> {
+            initForex();
+
+            swipeRefreshLayout1.setRefreshing(false);
+        });
+    }
+
+    public String formatNumber(double number, String format) {
+        DecimalFormat decimalFormat = new DecimalFormat(format);
+        return decimalFormat.format(number);
+    }
+}
